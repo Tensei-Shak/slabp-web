1f0e2ab85546d3d5f8cd3b2c689035f218ac8a84
Tensei-Shak <lnieto009@gmail.com>
2025-03-23 15:45:28 -0500

    Agregar archivos iniciales para la configuración del proyecto Vue con Vite, incluyendo componentes, estilos y configuración de TypeScript.

---
+++ src/components/AuthForm.vue
@@ -0,0 +1,20 @@
+<template>
+    <div></div>
+</template>
+<script lang='ts'>
+import { defineComponent, type PropType } from 'vue';
+export default defineComponent({
+    name: 'AuthForm',
+    props: {
+        isModeForm: {
+            type: String as PropType<string>,
+            required: true,
+        }
+    },
+    setup() {
+        //Start Coding
+        return {}
+    },
+})
+</script>
+<style scoped></style>
